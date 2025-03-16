617e464a93bbd5542376d1bd7cd83df6726ad231
unknown <marccejg@hotmail.com>
2025-03-15 21:32:41 -0300

    primer commit

---
+++ auto.ts
@@ -0,0 +1,26 @@
+export class Auto{
+    
+    marca:string;
+    modelo:string;
+    anio:number;
+    ascelerar:boolean;
+    frenar:boolean;
+
+    
+    constructor(pMarca:string,pModelo:string,pAnio:number,pAscelerar:boolean,pFrenar:boolean){
+        this.marca = pMarca;
+        this.modelo = pModelo;
+        this.anio = pAnio;
+        this.ascelerar = pAscelerar;
+        this.frenar = pFrenar;
+
+    }
+
+
+    estaAscelerando(pAscelerar:boolean):void{
+        if(this.ascelerar==true){
+        console.log("El auto está Ascelerando ");}
+    }
+
+    
+}
---
+++ index.ts
@@ -0,0 +1,35 @@
+import { Auto } from "./auto";
+
+let primerAuto = new Auto("Audi","A3",2020,true,false);
+let segundoAuto = new Auto("Ford","Fiesta",2015,false,true);
+let tercerAuto = new Auto("Chevrolet","Corsa",2008,true,false);
+let cuartoAuto = new Auto("Fiat","Uno",1999,true,false);
+
+
+console.log(primerAuto);
+console.log(segundoAuto);
+console.log(tercerAuto);
+
+
+/*primerAuto.marca = "LG";
+/*primerAuto.esSmart = true;
+primerAuto.estaPrendido = false;
+primerAuto.precio = 500000;
+primerAuto.pulgadas = 60;
+primerAuto.resolucion = "UltraHD";
+primerAuto.tipoPantalla = "Plana";
+*/
+
+
+
+
+
+/*
+   pulgadas:number;
+    esSmart:boolean;
+    resolucion:string;
+    tipoPantalla:string;
+    estaPrendido:boolean;
+    precio:number;
+    marca:string;
+*/
